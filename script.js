8b1fa9892025e1c9b3d9b6a1ba3e630f0b1e2cbd
Hussein-Graphix <husaynomar2020@gmail.com>
2026-05-07 12:52:33 +0200

    -m google add

--- script.js
+++ script.js
@@ -308,3 +308,12 @@ window.addEventListener('scroll', () => {
 
 renderProducts();
 renderCart();
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-L3WSL8T2XT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-L3WSL8T2XT');
+</script>
